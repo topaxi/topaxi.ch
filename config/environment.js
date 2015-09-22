a974d6f4152833580f5d945c8271909025d6764f
Damian Senn <damian.senn@gmail.com>
2015-09-22 19:43:28 +0200

    Put on /new for now

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'topaxich',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/new',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
