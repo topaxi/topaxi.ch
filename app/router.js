ed3f2b5d3ee7cd2414fb91394ccd0b23db2c28f2
Damian Senn <damian.senn@gmail.com>
2015-09-22 19:41:47 +0200

    Update!

--- app/router.js
+++ app/router.js
@@ -1,11 +1,14 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
-var Router = Ember.Router.extend({
+const Router = Ember.Router.extend({
   location: config.locationType
-});
+})
 
 Router.map(function() {
-});
+  this.route('index', { path: '/' })
+  this.route('about')
+  this.route('example-post')
+})
 
 export default Router;
