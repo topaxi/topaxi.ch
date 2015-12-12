e700432b789a556a14127625c24b3aa3d51ca3c7
Damian Senn <damian.senn@gmail.com>
2015-12-12 16:53:08 +0100

    Don't transform urls, fixes font paths

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    cssnextOptions: {
+      url: false
+    },
     fingerprint: {
       exclude: [
         'sleepy-topcatinja__dark-eyes.png',
