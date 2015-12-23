f009dee13ceac6fa371c0c8cdc566a4023f25fe4
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-12-23 08:34:00 +0100

    Move portfolio data in separate files

---
+++ app/portfolio/technologies.js
@@ -0,0 +1,78 @@
+export default {
+  'ember': {
+    name: 'Ember.js',
+    link: 'http://emberjs.com/'
+  },
+  'nodejs': {
+    name: 'Node.js',
+    link: 'http://nodejs.org/'
+  },
+  'django': {
+    name: 'Django',
+    link: 'http://djangoproject.com/'
+  },
+  'jsonapi': {
+    name: 'JSON:API',
+    link: 'http://jsonapi.org/'
+  },
+  'cssnext': {
+    name: 'cssnext',
+    link: 'http://cssnext.io/'
+  },
+  'socketio': {
+    name: 'socket.io',
+    link: 'http://socket.io/'
+  },
+  'php': {
+    name: 'PHP',
+    link: null
+  },
+  'javascript': {
+    name: 'JavaScript',
+    link: null
+  },
+  'css': {
+    name: 'CSS',
+    link: null
+  },
+  'css3': {
+    name: 'CSS3',
+    link: null
+  },
+  'html5': {
+    name: 'HTML5',
+    link: null
+  },
+  'xhtml11': {
+    name: 'XHTML 1.1',
+    link: null
+  },
+  'python': {
+    name: 'Python',
+    link: null
+  },
+  'mysql': {
+    name: 'MySQL',
+    link: null
+  },
+  'pgsql': {
+    name: 'Postgres SQL',
+    link: null
+  },
+  'redis': {
+    name: 'Redis',
+    link: null
+  },
+  'postcss': {
+    name: 'PostCSS',
+    link: null
+  },
+  'broccolijs': {
+    name: 'Broccoli.js',
+    link: null
+  },
+  'closurecompiler': {
+    name: 'Closure Compiler',
+    link: null
+  }
+}
