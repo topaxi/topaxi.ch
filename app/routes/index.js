9366bf78e0ea31b9e7835b2f366e85c5a2aa5070
Damian Senn <senn@puzzle.ch>
2016-03-04 07:33:12 +0100

    Make index work in fastboot

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,16 @@ import $             from 'jquery'
 import Route         from 'ember-route'
 import injectService from 'ember-service/inject'
 
+const isFastboot = typeof najax !== 'undefined'
+
 export default Route.extend({
-  ajax: injectService(),
+  ajax: isFastboot ? null : injectService(),
 
   model() {
+    if (isFastboot) {
+      return
+    }
+
     return this.get('ajax')
       .request('https://topaxi.codes/', { dataType: 'html' })
       .then(data      => data.replace(/href="\//g, 'href="https://topaxi.codes/'))
