aa94bd79650d7a8fa076d7692d5d3a179a316d03
Damian Senn <senn@puzzle.ch>
2017-03-17 09:47:55 +0100

    Enable babel loose mode

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    babel: {
+      loose: true
+    },
     cssnextOptions: {
       url: false
     },
